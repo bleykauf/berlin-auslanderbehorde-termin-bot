c07e5f93939ec5a1f1888bf96a7dea8ee5d4341c
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2024-05-05 13:31:53 +0200

    update requirements, fix formatting

--- berlin_bot.py
+++ berlin_bot.py
@@ -1,20 +1,19 @@
-import time
-import os
 import logging
+import os
+import time
 from platform import system
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-
 
 system = system()
 
 logging.basicConfig(
-    format='%(asctime)s\t%(levelname)s\t%(message)s',
+    format="%(asctime)s\t%(levelname)s\t%(message)s",
     level=logging.INFO,
 )
+
 
 class WebDriver:
     def __init__(self):
@@ -24,29 +23,42 @@ class WebDriver:
     def __enter__(self) -> webdriver.Chrome:
         logging.info("Open browser")
         # some stuff that prevents us from being locked out
-        options = webdriver.ChromeOptions() 
-        options.add_argument('--disable-blink-features=AutomationControlled')
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable-blink-features=AutomationControlled")
         self._driver = webdriver.Chrome(options=options)
-        self._driver.implicitly_wait(self._implicit_wait_time) # seconds
-        self._driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self._driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+        self._driver.implicitly_wait(self._implicit_wait_time)  # seconds
+        self._driver.execute_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"  # noqa: E501
+        )
+        self._driver.execute_cdp_cmd(
+            "Network.setUserAgentOverride",
+            {
+                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36"  # noqa: E501
+            },
+        )
         return self._driver
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         logging.info("Close browser")
         self._driver.quit()
 
+
 class BerlinBot:
     def __init__(self):
         self.wait_time = 20
         self._sound_file = os.path.join(os.getcwd(), "alarm.wav")
-        self._error_message = """Für die gewählte Dienstleistung sind aktuell keine Termine frei! Bitte"""
+        self._error_message = (
+            """Für die gewählte Dienstleistung sind aktuell keine Termine frei! Bitte"""
+        )
 
     @staticmethod
     def enter_start_page(driver: webdriver.Chrome):
         logging.info("Visit start page")
         driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen")
-        driver.find_element(By.XPATH, '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a').click()
+        driver.find_element(
+            By.XPATH,
+            '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a',  # noqa: E501
+        ).click()
         time.sleep(5)
 
     @staticmethod
@@ -54,20 +66,20 @@ class BerlinBot:
         logging.info("Ticking off agreement")
         driver.find_element(By.XPATH, '//*[@id="xi-div-1"]/div[4]/label[2]/p').click()
         time.sleep(1)
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+        driver.find_element(By.ID, "applicationForm:managedForm:proceed").click()
         time.sleep(5)
 
     @staticmethod
     def enter_form(driver: webdriver.Chrome):
         logging.info("Fill out form")
         # select china
-        s = Select(driver.find_element(By.ID, 'xi-sel-400'))
+        s = Select(driver.find_element(By.ID, "xi-sel-400"))
         s.select_by_visible_text("China")
         # eine person
-        s = Select(driver.find_element(By.ID, 'xi-sel-422'))
+        s = Select(driver.find_element(By.ID, "xi-sel-422"))
         s.select_by_visible_text("eine Person")
         # no family
-        s = Select(driver.find_element(By.ID, 'xi-sel-427' ))
+        s = Select(driver.find_element(By.ID, "xi-sel-427"))
         s.select_by_visible_text("nein")
         time.sleep(5)
 
@@ -76,25 +88,28 @@ class BerlinBot:
         time.sleep(2)
 
         # click on study group
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
+        driver.find_element(
+            By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p'
+        ).click()
         time.sleep(2)
 
         # b/c of stufy
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
+        driver.find_element(
+            By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label'
+        ).click()
         time.sleep(4)
 
         # submit form
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+        driver.find_element(By.ID, "applicationForm:managedForm:proceed").click()
         time.sleep(10)
-    
+
     def _success(self):
         logging.info("!!!SUCCESS - do not close the window!!!!")
         while True:
             self._play_sound_osx(self._sound_file)
             time.sleep(15)
-        
-        # todo play something and block the browser
 
+        # todo play something and block the browser
 
     def run_once(self):
         with WebDriver() as driver:
@@ -104,10 +119,12 @@ class BerlinBot:
 
             # retry submit
             for _ in range(10):
-                if not self._error_message in driver.page_source:
+                if self._error_message not in driver.page_source:
                     self._success()
                 logging.info("Retry submitting form")
-                driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+                driver.find_element(
+                    By.ID, "applicationForm:managedForm:proceed"
+                ).click()
                 time.sleep(self.wait_time)
 
     def run_loop(self):
@@ -118,7 +135,7 @@ class BerlinBot:
             self.run_once()
             time.sleep(self.wait_time)
 
-    # stolen from https://github.com/JaDogg/pydoro/blob/develop/pydoro/pydoro_core/sound.py
+    # stolen from https://github.com/JaDogg/pydoro/blob/develop/pydoro/pydoro_core/sound.py # noqa: E501
     @staticmethod
     def _play_sound_osx(sound, block=True):
         """
@@ -130,9 +147,10 @@ class BerlinBot:
         http://stackoverflow.com/a/34568298/901641
         I never would have tried using AppKit.NSSound without seeing his code.
         """
+        from time import sleep
+
         from AppKit import NSSound
         from Foundation import NSURL
-        from time import sleep
 
         logging.info("Play sound")
         if "://" not in sound:
@@ -149,6 +167,7 @@ class BerlinBot:
 
         if block:
             sleep(nssound.duration())
+
 
 if __name__ == "__main__":
     BerlinBot().run_loop()
